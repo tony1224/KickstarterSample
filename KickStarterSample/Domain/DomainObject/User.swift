3723e25b3c4e095c91036f0097d6c97b68010ad5
kamui_project <starmine2013@gmail.com>
2018-07-17 00:26:28 +0900

    :recycle: Podsファイル対応
    Issue No: なし
    Description: なし

---
+++ KickStarterSample/Domain/DomainObject/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  Domain
+//
+//  Created by Morita Jun on 2018/07/16.
+//  Copyright © 2018 Morita Jun. All rights reserved.
+//
+
+import Foundation
+
+public struct User {
+    
+}
--- KickStarterSample/KickStarterSample/Displays/Home/ViewModels/HomeViewModel.swift
+++ KickStarterSample/KickStarterSample/Displays/Home/ViewModels/HomeViewModel.swift
@@ -7,13 +7,20 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import Domain
 
 protocol HomeViewModelInputs {
-    func
+    func viewDidAppear()
+    func changeSearchText(_ text: String?)
+    func didTapUserCell(_ user: User)
 }
 
 protocol HomeViewModelOutputs {
-    
+    var isLoadingIndicator: Signal<Bool> { get }
+    var users: Signal<[User]> { get }
+    var resignFirstResponder: Signal<Void> { get }
 }
 
 protocol HomeViewModelType {
@@ -23,5 +30,22 @@ protocol HomeViewModelType {
 
 /// ホーム画面のViewModelです。
 final class HomeViewModel: HomeViewModelType, HomeViewModelInputs, HomeViewModelOutputs {
+//    var inputs: HomeViewModelInputs = HomeViewModelInputs()
+//    var outputs: HomeViewModelOutputs = HomeViewModelOutputs()
+    
 
+    // MARK: - Inputs
+
+    func viewDidAppear() {
+        
+    }
+    
+    func changeSearchText(_ text: String?) {
+        
+    }
+    
+    func didTapUserCell(_ user: User) {
+        
+    }
+    
 }
